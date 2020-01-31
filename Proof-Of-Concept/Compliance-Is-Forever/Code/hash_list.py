0e516c13bbc452194442324ec9287690b316a81a
mmurto <mikko.murto@gmail.com>
2020-01-31 10:01:59 +0200

    Update hash_list.py and add example files.

--- Proof-Of-Concept/Compliance-Is-Forever/Code/hash_list.py
+++ Proof-Of-Concept/Compliance-Is-Forever/Code/hash_list.py
@@ -12,6 +12,16 @@ import glob
 # Buffer size for hash calculation.
 BUF_SIZE = 65536
 
+debug_dict = {
+    'hash_list': [],
+    'elf_list': [],
+    'source_list': [],
+    'no_hash_found': [],
+    'no_hash_found_built_in': [],
+    'no_hash_found_not_built_in': [],
+    'duplicate_hash_found': [], 
+}
+
 # Build pkgdata folder.
 pkgdata_folder = "/home/yocto/poky/build/tmp/pkgdata/qemux86-64/"
 
@@ -33,6 +43,8 @@ def calculate_sha256(file):
             if not data:
                 break
             sha256.update(data)
+    if debug:
+        debug_dict['hash_list'].append(sha256.hexdigest())
     return sha256.hexdigest()
 
 # Try to calculate hash for all possible source file locations.
@@ -97,12 +109,31 @@ for file in os.listdir(pkgdata_folder):
                 hash_dictionary[file][elf_file] = {}
                 elf_file_folder = "/".join(elf_file.split("/", 10)[:10])
 
+                if debug:
+                    debug_dict['elf_list'].append(elf_file)
+
                 # Loop over all source files of the ELF file.
                 for source_file in source_files:
                     hash_dictionary[file][elf_file][source_file] = try_sha256(elf_file_folder, source_file)
                    
                     if debug:
                         hash_dictionary[file][elf_file][source_file] = debug_sha256(elf_file_folder, source_file)
+                        debug_dict['source_list'].append(source_file)
+
+                        if not hash_dictionary[file][elf_file][source_file]['package'] and not hash_dictionary[file][elf_file][source_file]['recipe-sysroot'] and not hash_dictionary[file][elf_file][source_file]['recipe-sysroot-native'] and not hash_dictionary[file][elf_file][source_file]['root']:
+                            debug_dict['no_hash_found'].append(source_file)
+                        
+                        hashes = []
+                    
+                        for location, hash_value in hash_dictionary[file][elf_file][source_file].items():
+                            if hash_value:
+                                hashes.append(hash_value)
+                        
+                        if len(set(hashes)) > 1:
+                            debug_dict['duplicate_hash_found'].append(hash_dictionary[file][elf_file][source_file])
+                            
+
+
 
 print()
 
@@ -110,3 +141,54 @@ print()
 print('Writing data to hash_list.json.')
 with open('hash_list.json', 'w') as outfile:
     json.dump(hash_dictionary, outfile, indent=4, sort_keys=True)
+
+
+
+if debug:
+    print(f'Total elf files: {len(debug_dict["elf_list"])}.')
+    print(f'Unique elf files: {len(set(debug_dict["elf_list"]))}')
+    print(f'Total source files: {len(debug_dict["source_list"])}.')
+    print(f'Unique source files: {len(set(debug_dict["source_list"]))}.')
+    print(f'Total hash values: {len(debug_dict["hash_list"])}.')
+    print(f'Unique hash values: {len(set(debug_dict["hash_list"]))}.')
+    print(f'Sources, no hash: {len(debug_dict["no_hash_found"])}.')
+    print(f'Sources, no hash, unique: {len(set(debug_dict["no_hash_found"]))}.')
+
+    no_hash_found_built_in = []
+    for source in debug_dict['no_hash_found']:
+        if '<built-in>' in source:
+            debug_dict['no_hash_found_built_in'].append(source)
+        if '<built-in>' not in source:
+            debug_dict['no_hash_found_not_built_in'].append(source)
+
+    print(f'Sources, no hash, <built-in>: {len(debug_dict["no_hash_found_built_in"])}.')
+    print(f'Sources, no hash, <built-in>, unique: {len(set(debug_dict["no_hash_found_built_in"]))}.')
+    print(f'Sources, no hash, not <built-in>: {len(debug_dict["no_hash_found_not_built_in"])}.')
+    print(f'Sources, no hash, not <built-in>, unique: {len(set(debug_dict["no_hash_found_not_built_in"]))}.')
+    print(f'Sources, conflicting hash values: {len(debug_dict["duplicate_hash_found"])}')
+    
+    new_list = []
+    counts = []
+
+    for dict_ in debug_dict["duplicate_hash_found"]:
+        try:
+            i = new_list.index(dict_)
+        except ValueError:
+            counts.append(1)
+            new_list.append(dict_)
+        else:
+            counts[i] += 1
+
+    assert len(counts) == len(new_list)
+    for dict_, count in zip(new_list, counts):
+        dict_['cnt'] = count
+
+    del counts
+
+    debug_dict["duplicate_hash_found_unique"] = new_list
+
+    print(f'Sources, conflicting hash values, unique: {len(debug_dict["duplicate_hash_found_unique"])}.')
+
+    print('Writing debug data to debug.json.')
+    with open('debug.json', 'w') as outfile:
+        json.dump(debug_dict, outfile, indent=4, sort_keys=True)
